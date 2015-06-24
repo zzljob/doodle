78bce9280db7e764e45597d8583fef3e82a3ddd5
J.O.B <zzljob@gmail.com>
2015-06-24 18:08:35 +0800

    Android Material Design做一个Toolbar

--- app/src/main/java/com/yepstudio/android/doodle/MainActivity.java
+++ app/src/main/java/com/yepstudio/android/doodle/MainActivity.java
@@ -20,10 +20,12 @@ package com.yepstudio.android.doodle;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.yepstudio.android.library.doodle.Doodle;
 
@@ -33,12 +35,17 @@ import com.yepstudio.android.library.doodle.Doodle;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
     private Button mBtnGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        this.setSupportActionBar(mToolbar);
+
         mBtnGo = (Button) findViewById(R.id.btn_go);
         mBtnGo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,8 +67,10 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
+            Toast.makeText(this, "设置", Toast.LENGTH_SHORT).show();
             return true;
         } else if (id == R.id.action_clear) {
+            Toast.makeText(this, "清空", Toast.LENGTH_SHORT).show();
             return true;
         }
 
